cbcd565ea52eb1b150231829d4de834b4291596a
Enric Pina <epina87@gmail.com>
2023-08-01 14:29:52 +0200

    change refactor new user

---
+++ src/layout/auth/Signup/formUtils.js
@@ -0,0 +1,50 @@
+import { signUp } from "../../../api/auth";
+
+export const isButtonDisabled = (user, email, password, passwordConfirm) => {
+  if (
+    !!user?.length &&
+    !!email?.length &&
+    !!password?.length &&
+    !!passwordConfirm?.length
+  ) {
+
+    
+
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export const isPasswordEqual = (password, passwordConfirm) => {
+  if (password !== passwordConfirm) {
+    return 'password confirmation does not match';
+  } else {
+    return '';
+  }
+};
+
+export const createNewUser = async (user,email,password)=>{
+
+    const data = {
+        user,
+        email,
+        password,
+      };
+
+      try {
+        const newUser = await signUp(data, {
+            headers: { 'content-type': 'multipart/form-data' },
+          });
+  
+        return newUser
+         
+
+      } catch (error) {
+        
+          return error
+      }
+
+    
+
+}
