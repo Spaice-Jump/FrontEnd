76865fcfb3a55761027cf29bd1dbb1bd31839519
Antonio Luis Martinez <antonillo_1995@hotmail.com>
2023-09-14 17:45:09 +0200

    added more translations on main page

--- src/layout/home/components/ProjectsSection.js
+++ src/layout/home/components/ProjectsSection.js
@@ -2,8 +2,10 @@ import React from "react";
 import allPlanets from "../../../assets/img/all-planets.jpg";
 import newEnvironments from "../../../assets/img/new-environments.jpg";
 import differentShips from "../../../assets/img/different-ships.jpg";
+import { useTranslation } from "react-i18next";
 
 function ProjectsSection() {
+  const { t } = useTranslation();
   return (
     <section className="projects-section bg-light" id="projects">
       {
@@ -15,10 +17,8 @@ function ProjectsSection() {
             </div>
             <div className="col-xl-4 col-lg-5">
               <div className="featured-text text-center text-lg-left">
-                <h4>¡Viaja por todo el sistema solar!</h4>
-                <p className="text-black-50 mb-0">
-                  Prepárate para un emocionante viaje interplanetario que te llevará a recorrer cada rincón de nuestro sistema solar, desde el ardiente sol de Mercurio hasta las heladas lunas de Neptuno. ¡Descubre los secretos y maravillas de este asombroso viaje cósmico!
-                </p>
+                <h4>{t("projects_section.title-1")}</h4>
+                <p className="text-black-50 mb-0">{t("projects_section.text-1")}</p>
               </div>
             </div>
           </div>
@@ -31,10 +31,8 @@ function ProjectsSection() {
               <div className="bg-black text-center h-100 project">
                 <div className="d-flex h-100">
                   <div className="project-text w-100 my-auto text-center text-lg-left">
-                    <h4 className="text-white">¡Descubre nuevos mundos y paisajes admirables!</h4>
-                    <p className="mb-0 text-white-50">
-                      Adéntrate en un viaje lleno de sorpresas mientras exploras nuevos mundos y paisajes admirables. Desde exóticos parajes terrestres hasta inexploradas maravillas extraterrestres, te esperan experiencias asombrosas en cada rincón de nuestro universo.
-                    </p>
+                    <h4 className="text-white">{t("projects_section.title-2")}</h4>
+                    <p className="mb-0 text-white-50">{t("projects_section.text-2")}</p>
                   </div>
                 </div>
               </div>
@@ -49,10 +47,8 @@ function ProjectsSection() {
               <div className="bg-black text-center h-100 project">
                 <div className="d-flex h-100">
                   <div className="project-text w-100 my-auto text-center text-lg-right">
-                    <h4 className="text-white">¡Viaja en todo tipo de naves!</h4>
-                    <p className="mb-0 text-white-50">
-                      Prepárate para una emocionante aventura en la que podrás viajar en todo tipo de naves. Desde naves espaciales de alta tecnología hasta naves clásicas y exóticas, este viaje te llevará a explorar el espacio de la manera más espectacular y variada posible. ¡Embárcate en una odisea intergaláctica única!
-                    </p>
+                    <h4 className="text-white">{t("projects_section.title-3")}</h4>
+                    <p className="mb-0 text-white-50">{t("projects_section.text-3")}</p>
                   </div>
                 </div>
               </div>
