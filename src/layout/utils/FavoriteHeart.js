7da822f5e6bc91c04f8f14148a5ceab767a77d22
Antonio Luis Martinez <antonillo_1995@hotmail.com>
2023-09-20 18:42:11 +0200

    changed fav style

--- src/layout/utils/FavoriteHeart.js
+++ src/layout/utils/FavoriteHeart.js
@@ -28,7 +28,7 @@ function FavoriteHeart({ travelId, checked }) {
 
   return (
     <div>
-      <span onClick={toggleFavorite}>{isFavorite ? 'Añadido a favoritos ❤️' : 'Añadir a favoritos 🤍'}</span>
+      <button className='fav-hearth' onClick={toggleFavorite}>{isFavorite ? 'Añadido a favoritos ❤️' : 'Añadir a favoritos 🤍'}</button>
     </div>
   );
 }
